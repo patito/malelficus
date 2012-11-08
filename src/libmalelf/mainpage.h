c95e6eba19bafec15a0dbd49481532fd83fc2512
Tiago Natel de Moura <tiago4orion@gmail.com>
2012-11-08 09:17:00 -0200

    Added Documentation and tests to force -m32

---
+++ src/libmalelf/mainpage.h
@@ -0,0 +1,19 @@
+#ifndef MAINPAGE_H
+#define MAINPAGE_H
+
+/** @file mainpage.h
+* @brief Definition of class Template
+*
+*/ 
+/** \mainpage Malelficus ELF Library
+*
+* @authors i4k
+*
+* @section Introduction
+*
+* LibMalelf is part of MalELFicus toolkit.
+* The library can be used for malware analysis or reverse engineering of ELF binaries.
+* 
+*/
+
+#endif
